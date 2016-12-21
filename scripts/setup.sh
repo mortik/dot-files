193641cc73b58d1b7b9cadadc67bfa5bfde7a7b0
Marten Klitzke <me@mortik.xyz>
2016-12-21 16:27:44 +0100

    update setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
+echo
+echo "Starting Dotfiles Setup..."
+echo
+
 # brew
+echo
+echo "Setup Brew"
+echo
 which -s brew
 if [[ $? != 0 ]] ; then
     # Install Homebrew
@@ -12,6 +19,9 @@ fi
 brew install git tig bash-completion ansible hub michaeldfallen/formula/git-radar
 
 # BashFiles
+echo
+echo "Setup Bashrc, Aliases and Prompt"
+echo
 rm ~/.bashrc 2> /dev/null
 ln -s ~/.dotfiles/.bashrc ~/
 rm ~/.bash_aliases 2> /dev/null
@@ -22,6 +32,9 @@ rm ~/.bash_prompt 2> /dev/null
 ln -s ~/.dotfiles/.bash_prompt ~/
 
 # git
+echo
+echo "Setup git"
+echo
 rm ~/.gitignore_global 2> /dev/null
 ln -s ~/.dotfiles/.gitignore_global ~/
 rm ~/.gitmessage.txt 2> /dev/null
@@ -32,6 +45,9 @@ if [ ! -f ~/.gitconfig ]; then
 fi
 
 # atom
+echo
+echo "Setup atom"
+echo
 rm ~/.atom/*.cson 2> /dev/null
 ln -s ~/.dotfiles/atom/*.cson ~/.atom/
 rm ~/.atom/*.coffee 2> /dev/null
@@ -41,7 +57,27 @@ ln -s ~/.dotfiles/atom/*.less ~/.atom/
 apm install --packages-file ~/.dotfiles/atom/packages.list
 
 # others
+echo
+echo "Setup vim and tig"
+echo
 rm ~/.vimrc 2> /dev/null
 ln -s ~/.dotfiles/.vimrc ~/
 rm ~/.tigrc 2> /dev/null
 ln -s ~/.dotfiles/.tigrc ~/
+
+read -p "Setup Github Auth-Token? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  # Read Password
+  echo -n Token:
+  read -s token
+  echo
+  git config --global github.token ${token}
+  echo
+  echo "Github Auth-Token configured"
+fi
+
+echo
+echo "Setup Finished!"
+echo
