58f93b2bfd4765be0fff1e12eaea021f54e80dd3
Marten Klitzke <marten.klitzke@sumcumo.com>
2017-05-05 14:07:06 +0200

    chore(setup): update brew

--- scripts/run.sh
+++ scripts/run.sh
@@ -7,8 +7,10 @@ echo "Starting Provisioning..."
 echo
 
 tags=""
-if [ -n "$1" ]; then
-  tags="-t $1"
+tag_names="$@"
+echo $tag_names
+if [ -n "${tag_names}" ]; then
+  tags="-t $tag_names"
 fi
 
 ansible-playbook base.yml -e @vault.yml --ask-sudo-pass $tags
