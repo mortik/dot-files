2f664d83ab6391c34a75c277b71cfbb43d9084b7
Marten Klitzke <me@mortik.xyz>
2017-12-15 15:55:55 +0100

    Fix hosts file

--- scripts/run.sh
+++ scripts/run.sh
@@ -13,7 +13,7 @@ if [ -n "${tag_names}" ]; then
   tags="-t $tag_names"
 fi
 
-ansible-playbook base.yml -e @vault.yml --ask-sudo-pass $tags
+ansible-playbook base.yml -i hosts -e @vault.yml --ask-sudo-pass $tags
 
 echo
 echo "Provisioning Finished!"
